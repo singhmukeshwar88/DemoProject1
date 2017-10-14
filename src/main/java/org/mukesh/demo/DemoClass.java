fc51b0477217eb1c38195a9f9eb770e835544780
Mukeshwar Singh <mukeshwar.singh@synechron.com>
2017-10-14 15:18:21 +0530

    all commit

---
+++ src/main/java/org/mukesh/demo/DemoClass.java
@@ -0,0 +1,13 @@
+package org.mukesh.demo;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("demo class main function");
+		
+
+	}
+
+}
